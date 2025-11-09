24ac2b9efaa9162bca767668ce204264644e701a
Ben Monaghan <monoganog@gmail.com>
2025-11-09 19:41:04 +0000

    Update module source and version in main.tf
    Updated module source URL and version in main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ provider "tfe" {
 
 
 module "terraform_lz_bottom_level_module" {
+  // Replace this URL with the one from your own private module registry
   source  = "app.terraform.io/ben-monaghan-org/Terraform-LZ-Bottom-Level-Module/tfe"
   version = "~> 1.0.1"
 
